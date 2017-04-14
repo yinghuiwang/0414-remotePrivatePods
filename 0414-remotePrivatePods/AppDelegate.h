97175408f6a7093e3efacb839484ffc76aef0385
王英辉 <13373928865@qq.com>
2017-04-14 13:44:28 +0800

    创建cocoapods私有库测试

---
+++ 0414-remotePrivatePods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0414-remotePrivatePods
+//
+//  Created by 王英辉 on 2017/4/14.
+//  Copyright © 2017年 王英辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 0414-remotePrivatePods/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  0414-remotePrivatePods
+//
+//  Created by 王英辉 on 2017/4/14.
+//  Copyright © 2017年 王英辉. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
